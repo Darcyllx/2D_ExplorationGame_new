bced581d1d3e3c741ab4fa22146a447fedfa153f
Darcyllx <ll4371@nyu.edu>
2021-10-18 01:48:50 -0400

    10.18 2am

---
+++ Assets/HurtPlayer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+public class HurtPlayer : MonoBehaviour
+{
+    private HealthManager healthMan;
+    public float waitToHurt = 1f;
+    public bool isTouching;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthMan = FindObjectOfType<HealthManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (reloading)
+        {
+            waitToLoad -= Time.deltaTime;
+            if(waitToLoad <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        */
+
+        if (isTouching)
+        {
+            waitToHurt -= Time.deltaTime;
+            if(waitToHurt <= 0)
+            {
+                healthMan.HurtPlayer(10);
+                waitToHurt = 1f;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            ///other.gameObject.SetActive(false);
+            //reloading = true;
+            other.gameObject.GetComponent<HealthManager>().HurtPlayer(10);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if(other.collider.tag == "Player")
+        {
+            isTouching = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            isTouching = false;
+            waitToHurt = 2f;
+        }
+    }
+}
--- Assets/TextTrigger5.cs
+++ Assets/TextTrigger5.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TextTrigger5 : MonoBehaviour
 {
@@ -29,5 +30,6 @@ public class TextTrigger5 : MonoBehaviour
         yield return new WaitForSeconds(5);
         Destroy(uiObject);
         Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
