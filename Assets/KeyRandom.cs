4439c759b0a833f17fb5e40ef4bb56158f8dc604
Darcyllx <ll4371@nyu.edu>
2021-10-20 16:49:31 -0400

    10.20 4:50pm random keys (partially done

---
+++ Assets/KeyRandom.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRandom : MonoBehaviour
+{
+    public Transform[] points;
+    public float InvokeRate = 1.0f;
+    public GameObject Key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PickPoints();
+    }
+
+    // Update is called once per frame
+    void PickPoints()
+    {
+        int indexNumber = Random.Range(0, points.Length);
+        Key.transform.position = points[indexNumber].position;
+        //Instantiate(Key, points[indexNumber].position, Key.transform.rotation);
+    }
+}
